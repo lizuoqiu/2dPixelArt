4322ca3837726cc75fc752880c220a25dc634f1b
MaxChioa <maxchioa@gmail.com>
2024-03-22 17:16:29 -0700

    API relavent changes. Wrapped normal map generation into a function. Set up flask api. Front end still WIP.

--- python/passing_api.py
+++ python/passing_api.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
+from PIL import Image
 from io import BytesIO
 import base64
 import os
 
+from omnidata_main.omnidata_tools.torch.demo_func import get_normal_map
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 # Ensure there's a folder for uploads
 UPLOAD_FOLDER = 'uploads'
@@ -20,20 +25,21 @@ def upload_file():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
     if file:
-        file.save( file.filename)
+        file.save(file.filename)
         filename = secure_filename(file.filename)
         # filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         # file.save(filepath)
         # Process the file as needed
-        return jsonify({'message': 'File successfully uploaded', 'filename': filename}), 200
-
-@app.route('/get-normal-map')
-def get_normal_map(normal_map):
-    img_io = BytesIO()
-    normal_map.save(img_io, format="PNG")
-    img_io.seek(0)
-    img_data = base64.b64encode(img_io.getvalue()).decode('utf-8')
-    return jsonify({'image': f'data:image/png;base64,{img_data}'})
-
+        img = Image.open(file)
+        norm_map = get_normal_map('normal', img)
+        # norm_map
+        # img_io = BytesIO()
+        # norm_map.save(img_io, format="PNG")
+        # img_io.seek(0)
+        # img_data = base64.b64encode(img_io.getvalue()).decode('utf-8')
+        return jsonify({'image': f'./output.jpg'}), 200
+        
+        # return jsonify({'message': 'File successfully uploaded', 'filename': filename}), 200
+    
 if __name__ == '__main__':
     app.run(debug=True)
