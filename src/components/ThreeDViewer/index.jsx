64c513dcc88e4e27f3c4bc1a00f53503a4c04047
Crimson <1615226024@qq.com>
2024-04-03 12:47:33 -0700

    show bin image

--- src/components/ThreeDViewer/index.jsx
+++ src/components/ThreeDViewer/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import ThreeDViewerStyle from "./style"; // 保留样式import，以便您可以自定义图片和容器的样式
+import React, { useState, useEffect } from "react";
+import ThreeDViewerStyle from "./style";
 
-function ThreeDViewer({ imageUrl }) {
+function ThreeDViewer({ imageData }) {
+  const [imageSrc, setImageSrc] = useState(null);
+
+  useEffect(() => {
+    if (imageData) {
+      // 将ArrayBuffer转换为Blob
+      const blob = new Blob([imageData], { type: "image/png" }); // 假设图片格式为JPEG，根据实际情况调整
+      // 创建Blob URL
+      const url = URL.createObjectURL(blob);
+      setImageSrc(url);
+
+      // 清理函数：当组件卸载或者imageData变化时，释放URL对象
+      return () => URL.revokeObjectURL(url);
+    }
+  }, [imageData]);
+
   return (
     <ThreeDViewerStyle>
-      <img src={imageUrl} alt="Max,Crimson,Scoot" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+      {imageSrc && <img src={imageSrc} alt="123" style={{ maxWidth: "100%", maxHeight: "100%" }} />}
     </ThreeDViewerStyle>
   );
 }
