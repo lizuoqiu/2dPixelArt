7b4e1fe448c61b99b281f8e6a9ad759d1ba9ee9e
Crimson <1615226024@qq.com>
2024-04-16 14:35:44 -0700

    8Directions

--- src/components/PointCurve/index.jsx
+++ src/components/PointCurve/index.jsx
@@ -1,198 +1,96 @@
 import React, { Component, createRef } from "react";
-import { connect } from "react-redux";
-import { imageActions } from "store/image";
-import { selectors as imageSelectors } from "store/image";
-import PointCurveStyle from "./style";
-import { adjustTone, cloneCanvas } from "utils/canvasUtils";
 
-let circle1 = new Path2D();
-let circle2 = new Path2D();
-
-class PointCurve extends Component {
+class DirectionSelector extends Component {
   constructor() {
     super();
-    this.pointCurveRef = createRef();
+    this.directionRef = createRef();
   }
   state = {
-    initBoundingBox: null,
-    cpS: {
-      x: 0,
-      y: 0
-    },
-    cp1: {
-      x: 0,
-      y: 0
-    },
-    cp2: {
-      x: 0,
-      y: 0
-    },
-    cpE: {
-      x: 0,
-      y: 0
-    },
-    selectedControl: null
+    selectedDirection: null // Stores the currently selected direction
   };
-  isPointInPath = (x1, y1, x2, y2) => {
-    if (x2 >= x1 - 15 && x2 <= x1 + 15 && y2 >= y1 - 15 && y2 <= y1 + 15) {
-      return true;
-    }
-    return false;
-  };
-  drawPointCurve = () => {
-    const { cpS, cp1, cp2, cpE } = this.state;
-    const pointCurveCanvas = this.pointCurveRef.current;
-    const pointCurveContext = pointCurveCanvas.getContext("2d");
 
-    pointCurveContext.clearRect(0, 0, pointCurveCanvas.width, pointCurveCanvas.height);
-    pointCurveContext.beginPath();
-    pointCurveContext.moveTo(cpS.x, cpS.y);
-    pointCurveContext.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, cpE.x, cpE.y);
-    pointCurveContext.stroke();
-
-    // Start and end points
-
-    pointCurveContext.fillStyle = "blue";
-    pointCurveContext.beginPath();
-    pointCurveContext.arc(cpS.x, cpS.y, 5, 0, 2 * Math.PI); // Start point
-    pointCurveContext.arc(cpE.x, cpE.y, 5, 0, 2 * Math.PI); // End point
-    pointCurveContext.fill();
-
-    // Control points
-    circle1 = new Path2D();
-    circle2 = new Path2D();
-    circle1.arc(cp1.x, cp1.y, 5, 0, 2 * Math.PI); // Control point one
-    pointCurveContext.fillStyle = "red";
-    pointCurveContext.fill(circle1);
-    circle2.arc(cp2.x, cp2.y, 5, 0, 2 * Math.PI); // Control point two
-    pointCurveContext.fillStyle = "red";
-    pointCurveContext.fill(circle2);
+  drawDirections = () => {
+    const canvas = this.directionRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "50px Arial";
+    // Main Directions
+    ctx.fillText("↑", canvas.width / 2 - 15, 50); // Up
+    ctx.fillText("↓", canvas.width / 2 - 15, canvas.height - 20); // Down
+    ctx.fillText("←", 30, canvas.height / 2 + 15); // Left
+    ctx.fillText("→", canvas.width - 80, canvas.height / 2 + 15); // Right
+    // Diagonal Directions
+    ctx.fillText("↖", 30, 70); // Top-left
+    ctx.fillText("↗", canvas.width - 80, 70); // Top-right
+    ctx.fillText("↙", 30, canvas.height - 20); // Bottom-left
+    ctx.fillText("↘", canvas.width - 80, canvas.height - 20); // Bottom-right
   };
-  moveCurve = e => {
-    if (e.buttons !== 1) return;
-    const { selectedControl } = this.state;
-    if (selectedControl === "cp1") {
-      this.setState(
-        {
-          cp1: {
-            x: e.offsetX,
-            y: e.offsetY
-          }
-        },
-        () => {
-          this.drawPointCurve();
-        }
-      );
-    }
-    if (selectedControl === "cp2") {
-      this.setState(
-        {
-          cp2: {
-            x: e.offsetX,
-            y: e.offsetY
-          }
-        },
-        () => {
-          this.drawPointCurve();
-        }
-      );
-    }
-  };
-  selectCurve = e => {
-    const { cp1, cp2 } = this.state;
-    let sc = null;
-    if (this.isPointInPath(cp1.x, cp1.y, e.offsetX, e.offsetY)) {
-      sc = "cp1";
-    }
-    if (this.isPointInPath(cp2.x, cp2.y, e.offsetX, e.offsetY)) {
-      sc = "cp2";
-    }
-    this.setState({ selectedControl: sc });
-  };
+
   componentDidMount() {
-    const pointCurveCanvas = this.pointCurveRef.current;
-    this.setState(
-      {
-        cpS: { x: 0, y: pointCurveCanvas.width },
-        cp1: { x: 0, y: pointCurveCanvas.width },
-        cp2: { x: pointCurveCanvas.width, y: 0 },
-        cpE: { x: pointCurveCanvas.width, y: 0 }
-      },
-      () => {
-        this.drawPointCurve();
-        pointCurveCanvas.addEventListener("mousedown", this.selectCurve);
-        pointCurveCanvas.addEventListener("mousemove", this.moveCurve);
+    this.drawDirections();
+  }
+
+  selectDirection = e => {
+    const rect = this.directionRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const width = this.directionRef.current.width;
+    const height = this.directionRef.current.height;
+
+    // Using some margins for simplicity in direction detection
+    const margin = width * 0.1;
+
+    if (x < margin && y < margin) {
+      this.setState({ selectedDirection: "TOP-LEFT" });
+    } else if (x > width - margin && y < margin) {
+      this.setState({ selectedDirection: "TOP-RIGHT" });
+    } else if (x < margin && y > height - margin) {
+      this.setState({ selectedDirection: "BOTTOM-LEFT" });
+    } else if (x > width - margin && y > height - margin) {
+      this.setState({ selectedDirection: "BOTTOM-RIGHT" });
+    } else if (x > width * 0.3 && x < width * 0.7 && y < height * 0.3) {
+      this.setState({ selectedDirection: "UP" });
+    } else if (x > width * 0.3 && x < width * 0.7 && y > height * 0.7) {
+      this.setState({ selectedDirection: "DOWN" });
+    } else if (y > height * 0.3 && y < height * 0.7 && x < width * 0.3) {
+      this.setState({ selectedDirection: "LEFT" });
+    } else if (y > height * 0.3 && y < height * 0.7 && x > width * 0.7) {
+      this.setState({ selectedDirection: "RIGHT" });
+    }
+  };
+
+  sendDirectionToBackend = async () => {
+    try {
+      const response = await fetch("/api/direction", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ direction: this.state.selectedDirection })
+      });
+      if (response.ok) {
+        console.log("Direction sent successfully");
+      } else {
+        console.log("Failed to send direction");
       }
-    );
-  }
-  componentWillUnmount() {
-    const pointCurveCanvas = this.pointCurveRef.current;
-    pointCurveCanvas.removeEventListener("mousedown", this.selectCurve);
-    pointCurveCanvas.removeEventListener("mousemove", this.moveCurve);
-  }
+    } catch (error) {
+      console.error("Failed to send direction", error);
+    }
+  };
+
   render() {
-    const { pointCurveRef } = this;
-    const { cpS, cp1, cp2, cpE, selectedControl } = this.state;
-    let { cacheDepthCanvas, memoryDepthCanvas, parameters, operationStack, addEffect } = this.props;
     return (
-      <PointCurveStyle {...this.props.pointCurveProps}>
-        <canvas
-          width="200px"
-          height="200px"
-          ref={pointCurveRef}
-          onMouseOut={e => {
-            if (selectedControl) {
-              const { activeIndex, layerStack } = operationStack;
-              if (parameters.histogramParams.pixelRange && activeIndex > -1 && layerStack.length) {
-                addEffect({
-                  name: "depthStack",
-                  value: {
-                    func: adjustTone,
-                    params: [cloneCanvas(layerStack[activeIndex].bitmap), cpS, cp1, cp2, cpE]
-                  }
-                });
-              }
-            }
-          }}
-          // onMouseOver={e => selectedControl && this.setState({ selectedControl: null })}
-          onMouseUp={e => {
-            if (selectedControl) {
-              const { activeIndex, layerStack } = operationStack;
-              if (parameters.histogramParams.pixelRange && activeIndex > -1 && layerStack.length) {
-                let cacheCanvas = cacheDepthCanvas;
-                if (
-                  operationStack["depthStack"].length !== 0 &&
-                  operationStack["depthStack"][operationStack["depthStack"].length - 1].func.name.toString() !=
-                    "adjustTone"
-                ) {
-                  cacheCanvas = memoryDepthCanvas;
-                }
-                addEffect({
-                  name: "depthStack",
-                  value: {
-                    func: adjustTone,
-                    params: [cloneCanvas(layerStack[activeIndex].bitmap), cpS, cp1, cp2, cpE]
-                  }
-                });
-              }
-            }
-            this.setState({ selectedControl: null });
-          }}
-        ></canvas>
-      </PointCurveStyle>
+      <div>
+        <canvas ref={this.directionRef} width="250" height="250" onClick={this.selectDirection} />
+        {this.state.selectedDirection && (
+          <div>
+            <p>Selected Direction: {this.state.selectedDirection}</p>
+            <button onClick={this.sendDirectionToBackend}>Send Direction</button>
+          </div>
+        )}
+      </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  memoryDepthCanvas: imageSelectors.memoryDepthCanvas(state),
-  cacheDepthCanvas: imageSelectors.cacheDepthCanvas(state),
-  parameters: imageSelectors.parameters(state),
-  operationStack: imageSelectors.operationStack(state)
-});
-
-const mapDispatchToProps = {
-  addEffect: imageActions.addEffect
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PointCurve);
+export default DirectionSelector;
--- src/components/ThreeDViewer/index.jsx
+++ src/components/ThreeDViewer/index.jsx
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from "react";
 import ThreeDViewerStyle from "./style";
 
-function ThreeDViewer({ imageData }) {
-  const [imageSrc, setImageSrc] = useState(null);
+function ImageContainer() {
+  const [imageData, setImageData] = useState(null);
 
   useEffect(() => {
-    if (imageData) {
-      // 将ArrayBuffer转换为Blob
-      const blob = new Blob([imageData], { type: "image/png" }); // 假设图片格式为JPEG，根据实际情况调整
-      // 创建Blob URL
-      const url = URL.createObjectURL(blob);
-      setImageSrc(url);
+    const fetchImageData = async () => {
+      try {
+        const response = await fetch("https://your-backend-url/api/images");
+        const blob = await response.blob(); // 假设后端直接返回一个图片的Blob数据
+        setImageData(blob);
+      } catch (error) {
+        console.error("Failed to fetch image data:", error);
+      }
+    };
 
-      // 清理函数：当组件卸载或者imageData变化时，释放URL对象
-      return () => URL.revokeObjectURL(url);
-    }
-  }, [imageData]);
+    fetchImageData();
+  }, []);
 
   return (
-    <ThreeDViewerStyle>
-      {imageSrc && <img src={imageSrc} alt="123" style={{ maxWidth: "100%", maxHeight: "100%" }} />}
-    </ThreeDViewerStyle>
+    <div>
+      <h1>View 3D Image</h1>
+      {imageData && (
+        <img
+          src={URL.createObjectURL(imageData)}
+          alt="3D Visualization"
+          style={{ maxWidth: "100%", maxHeight: "100%" }}
+        />
+      )}
+    </div>
   );
 }
 
-export default ThreeDViewer;
+export default ImageContainer;
+
 //1. 8 directions
 //2. points movement
 //3. zoom in and out
