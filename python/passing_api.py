28b4e6ecd7447ebf960ea5e6f91358619b7e7e74
Lizuoqiu <bla127@sfu.ca>
2024-04-15 19:07:47 -0700

    finish initial function

--- python/passing_api.py
+++ python/passing_api.py
@@ -55,16 +55,16 @@ def upload_file():
         norm_map, shaded_image = apply_shading(img, raw_norm_map, light_sources, ambient_light)
         shaded_pil = Image.fromarray(shaded_image)
         shaded_pil.show()
-        # shaded_image.save(shaded_image_io, format="PNG")
+        shaded_image.save(shaded_image_io, format="PNG")
         norm_pil = Image.fromarray(np.uint8(norm_map))
         norm_pil.save(img_io, format="PNG")
         # image_data.save(img_io, format="PNG")
         img_io.seek(0)
         # shaded_image.seek(0)
         img_data = base64.b64encode(img_io.getvalue()).decode('utf-8')
-        # shaded_image_data = base64.b64encode(shaded_image.getvalue()).decode('utf-8')
-        # return jsonify({'normal_map': f'{img_data}'}, "shading_image", f'{shaded_image_data}'), 200
-        return jsonify({'normal_map': f'{img_data}'}), 200
+        shaded_image_data = base64.b64encode(shaded_image.getvalue()).decode('utf-8')
+        return jsonify({'normal_map': f'{img_data}'}, "shading_image", f'{shaded_image_data}'), 200
+        # return jsonify({'normal_map': f'{img_data}'}), 200
         #
         # return jsonify({'message': 'File successfully uploaded', 'filename': filename}), 200
 
